73ffb7fcbaa34ea01c4cb9f7cbc7ac8a94c084e9
revanli <justin_yufan@163.com>
2019-12-20 10:12:42 +0800

    feat: add bind snippet

--- src/jsvascript/bind/bind.js
+++ src/jsvascript/bind/bind.js
@@ -1,13 +1,36 @@
+/**
+ * bind函数思路
+ * 第一步：MDN定义： bind() 方法会创建一个函数，当这个新函数被调用时，第一
+ * 个参数作为它运行时的 this, 而其他参数作为新函数的参数，供调用时使用
+ * 第二步：返回的绑定函数可以做为构造函数使用，之前传入的第一个 this 参数失
+ * 效，提供的参数作为新函数的参数
+ */
+
 export default function (...bindArgs) {
   // 调用 bind 函数本身
   const self = this
   // 传进来的 this 对象
-  const context = bindArgs.shift()
+  let context = bindArgs.shift()
 
+  if (context == null) {
+    context = window
+  } else {
+    context = Object(context)
+  }
+
+  // 第一步 返回新函数
   const fBound = function(...args) {
-    // 函数运行并返回结果
+    // 第二步 当返回的新函数作为构造函数使用时，之前指定的 this 就会失效，// 此时this 指向实例对象
+    if (this instanceof fBound) {
+      context = this
+    }
+    // 第一步 函数可以运行并返回结果
+    // 第一步 传参做为新函数的参数
     return self.apply(context, [...bindArgs, ...args])
   }
+
+  // 第二步 修改返回函数的原型对象，实例对象就可以从原函数对象上继承属性和方法
+  fBound.prototype = Object.create(self.prototype)
 
   return fBound
 }
--- src/jsvascript/bind/bind.test.js
+++ src/jsvascript/bind/bind.test.js
@@ -18,4 +18,43 @@ describe('Function.prototype.bind', () => {
     expect(typeof newBar).toBe('function')
     expect(newBar(2)).toBe(bar.bind(foo, 3)(2));
   })
+
+  test('use return function as constructor, \'this\' points to the instance object ', () => {
+    const foo = { value: 1 }
+    function bar(name, age) {
+      this.name = name
+      this.age = age
+    }
+    // 原函数的远行对象的属性
+    bar.prototype.friend = 'revan'
+
+    const bindFoo = bar.bind(foo);  // 
+    const bindFoo2 = bar._bind(foo);
+    bindFoo2.prototype.address = 1;   // 修改返回函数的原型对象，增加属性
+
+    // 验证返回函数作为构造函数，实例对象会从原函数的原型对象上继承属性
+    expect(new bindFoo2().friend).toBe(new bindFoo().friend);
+
+    // 验证返回函数作为构造函数，之前绑定的 this 对象会失效， this 会指向
+    // 实例对象
+    expect(new bindFoo2().value).toBe(undefined);
+    expect(new bindFoo2().value).toBe(new bindFoo().value);
+
+    // 验证修改返回函数的原型对象，不会引起原始函数 bar 原型对象的修改
+    expect(bar.prototype.address).toBe(undefined)
+  })
+
+  test('when \'this\' argument is null or undefined, \'this\' points to window', () => {
+    window.value = 2;
+
+    function bar(age1, age2) {
+      age1 = age1 || 0;
+      age2 = age2 || 0;
+      return this.value + age1 + age2;
+    }
+
+    expect(bar._bind(null, 3)(1)).toBe(6)
+    expect(bar._bind(undefined, 3)(1)).toBe(6)
+  })
+
 })
